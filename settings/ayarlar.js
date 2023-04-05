611042fdb7f4f821185377337276ae2bec643789
burak <65681772+burak161@users.noreply.github.com>
2023-04-05 19:20:54 +0300

    Add files via upload

---
+++ settings/ayarlar.js
@@ -0,0 +1,7 @@
+require("dotenv").config();
+
+module.exports = {
+    TOKEN: process.env.TOKEN || "OTc3NjQwNDI5ODI4ODY2MDg4.G2Ytn7.dsm353N5-WSfeYsCWFGIXsWfT4fjsKI6PH5hsU",  // bot token gir :)
+    PREFIX: process.env.PREFIX || "!", // bot prefix
+    SAHİP: process.env.SAHİP || "592732420667736075", //sahip id :()
+}
